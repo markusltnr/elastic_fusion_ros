0fdfbb16c8ba670730c181c1ca9364827ad9b821
Markus Leitner <leitnermrks@gmail.com>
2021-10-18 10:55:58 +0200

    rosparam for command parameters

--- scripts/elastic_fusion_ros_server.py
+++ scripts/elastic_fusion_ros_server.py
@@ -20,7 +20,11 @@ class ElasticFusionROS:
         trajectories_file = os.path.join(plane_path, 'planes', 'tf.txt')
         camera_cfg_file = os.path.join(self.storage_path, 'camera_EF.cfg')
         output_file = os.path.join(self.storage_path, 'scene_'+str(goal.id))
-        cmd_elasticfusion = ["ElasticFusion",  "-l", klg_file, "-cal", camera_cfg_file, "d", "2", "-c", "15", "-cv", "1e-01", "-ie", "1e-05", "-pt", "60", "-f", "-q", "-name", output_file]
+        #cmd_elasticfusion = ["ElasticFusion",  "-l", klg_file, "-cal", camera_cfg_file, "d", "2", "-c", "15", "-cv", "1e-01", "-ie", "1e-05", "-pt", "60", "-f", "-q", "-name", output_file]
+        cmd_elasticfusion = ["ElasticFusion",  "-l", klg_file, "-cal", camera_cfg_file]
+        cmd_elasticfusion.extend(rospy.get_param('/elasticfusion/call_params', ["d", "2", "-c", "15", "-cv", "1e-01", "-ie", "1e-05", "-pt", "60", "-f", "-q"]))
+        cmd_elasticfusion.extend(["-name", output_file])
+        print(cmd_elasticfusion)
         
         ef = subprocess.Popen(cmd_elasticfusion)#,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         ef.wait()
